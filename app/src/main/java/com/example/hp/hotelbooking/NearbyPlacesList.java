7bcf162e4f95018271de5998eebc139d301f1114
kunai64 <anagani64@gmail.com>
2017-08-11 13:18:47 +0530

    Stable release with list of nearby places

--- app/src/main/java/com/example/hp/hotelbooking/NearbyPlacesList.java
+++ app/src/main/java/com/example/hp/hotelbooking/NearbyPlacesList.java
@@ -1,153 +1,138 @@
-package com.example.hp.hotelbooking;
+package com.example.hp.hotelbooking
 
-import android.os.AsyncTask;
-import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
-import android.widget.Toast;
+import android.os.AsyncTask
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import android.widget.Toast
 
-import org.apache.http.HttpResponse;
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
+import com.example.hp.hotelbooking.data.HotelAPIResponse
+import com.example.hp.hotelbooking.data.Result
+import com.google.gson.Gson
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.ArrayList;
-import java.util.List;
+import org.json.JSONException
+
+import java.io.BufferedReader
+import java.io.IOException
+import java.io.InputStream
+import java.io.InputStreamReader
+import java.net.HttpURLConnection
+import java.net.MalformedURLException
+import java.net.URL
 
 /**
  * Created by kunai on 3/8/17.
  */
 
-public class NearbyPlacesList extends AppCompatActivity {
+class NearbyPlacesList : AppCompatActivity() {
 
-    private double lat;
-    private double lang;
-    MainActivity ma = new MainActivity();
-    final String API_KEY = "AIzaSyD0vwXNlmIu_O4lAyCOB8imnnl3olpaVN8";
-    private RecyclerView recyclerView;
-    private nearbyAdapter mAdapter;
-    HttpResponse response;
-    ArrayList<content>venues;
-    @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.nearby);
+    internal val API_KEY = "AIzaSyD0vwXNlmIu_O4lAyCOB8imnnl3olpaVN8"
+    internal var ma = MainActivity()
+    private var lat: Double = 0.toDouble()
+    private var lang: Double = 0.toDouble()
+    private var recyclerView: RecyclerView? = null
+    private var mAdapter: nearbyAdapter? = null
 
-        lat = ma.getLats();
-        lang = ma.getLongs();
-        Toast.makeText(this, ""+ lat, Toast.LENGTH_SHORT).show();
-        googlePlaces g = new googlePlaces();
-        g.execute();
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.nearby)
+
+        lat = ma.getLats()
+        lang = ma.getLongs()
+        Toast.makeText(this, "" + lat, Toast.LENGTH_SHORT).show()
+        val g = googlePlaces()
+        g.execute()
     }
 
-    protected class googlePlaces extends AsyncTask<Void, Void, String> {
+    @Throws(JSONException::class)
+    private fun parseGoogleParse(response: String): List<Result> {
+        val resultArrayList: List<Result>
+        val gson = Gson()
+        val hotelAPIResponse = gson.fromJson<HotelAPIResponse>(response, HotelAPIResponse::class.java!!)
+        resultArrayList = hotelAPIResponse.results
+        return resultArrayList
+    }
 
-        String url;
+    protected inner class googlePlaces : AsyncTask<Void, Void, String>() {
+
+        internal var url: String
 
 
-        String inputString = null;
+        internal var inputString: String? = null
 
-        @Override
-        protected String doInBackground(Void... params) {
-            StringBuilder ba;
-            HttpURLConnection httpconn;
-            url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=22.72,75.83&type=hotel&radius=400&key=AIzaSyD0vwXNlmIu_O4lAyCOB8imnnl3olpaVN8";
-              StringBuffer sb = new StringBuffer(url);
+        override fun doInBackground(vararg params: Void): String? {
+            val ba: StringBuilder
+            val httpconn: HttpURLConnection
+            url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=22.72,75.83&type=hotel&radius=400&key=AIzaSyD0vwXNlmIu_O4lAyCOB8imnnl3olpaVN8"
+            val sb = StringBuffer()
             try {
-                String inputString = null;
-                URL urlconn = new URL(url);
-                httpconn = (HttpURLConnection) urlconn.openConnection();
-                httpconn.setRequestMethod("GET");
-                httpconn.connect();
-                InputStream is = httpconn.getInputStream();
-                BufferedReader bf = new BufferedReader(new InputStreamReader(is));
-                ba = new
-                        StringBuilder();
 
-                while ((bf.read() != -1)) {
-                    ba.append(inputString);
+                val urlconn = URL(url)
+                httpconn = urlconn.openConnection() as HttpURLConnection
+                httpconn.requestMethod = "GET"
+                httpconn.connect()
 
-                }
-                return ba.toString();
-            } catch (MalformedURLException e) {
-                e.printStackTrace();
-            } catch (IOException e) {
-                e.printStackTrace();
+                return readStream(httpconn.inputStream)
+
+            } catch (e: MalformedURLException) {
+                e.printStackTrace()
+            } catch (e: IOException) {
+                e.printStackTrace()
             }
-//            HttpClient httpClient = new DefaultHttpClient();
-//            HttpGet get = new HttpGet(url);
 
-            return null;
+            //            HttpClient httpClient = new DefaultHttpClient();
+            //            HttpGet get = new HttpGet(url);
+
+            return null
 
         }
 
-        @Override
-        protected void onPostExecute(String result) {
-            if(url == null){
-                Toast.makeText(getApplicationContext(), "No Results Found", Toast.LENGTH_SHORT).show();
+        // Converting InputStream to String
+
+        private fun readStream(`in`: InputStream): String {
+            var reader: BufferedReader? = null
+            val response = StringBuffer()
+            try {
+                reader = BufferedReader(InputStreamReader(`in`))
+                var line: String
+                while ((line = reader.readLine()) != null) {
+                    response.append(line)
+                }
+            } catch (e: IOException) {
+                e.printStackTrace()
+            } finally {
+                if (reader != null) {
+                    try {
+                        reader.close()
+                    } catch (e: IOException) {
+                        e.printStackTrace()
+                    }
+
+                }
             }
-            else {
+            return response.toString()
+        }
+
+        override fun onPostExecute(result: String?) {
+            if (result == null) {
+                Toast.makeText(applicationContext, "No Results Found", Toast.LENGTH_SHORT).show()
+            } else {
                 try {
-                    venues = parseGoogleParse(url);
-                    recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
-                    RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getApplicationContext());
-                    List listTitle = new ArrayList();
-                    for (int i = 0; i < venues.size(); i++) {
-                        // make a list of the venus that are loaded in the list.
-                        // show the name, the category and the city
-                        listTitle.add(i, venues.get(i).getName() + "\nOpen Now: " + venues.get(i).getOpenNow() );
-                    }
-                    mAdapter = new nearbyAdapter(listTitle);
+                    // venues = parseGoogleParse(result);
+                    recyclerView = findViewById(R.id.recyclerView) as RecyclerView
+                    val mLayoutManager = LinearLayoutManager(applicationContext)
 
-                    recyclerView.setLayoutManager(mLayoutManager);
-                    recyclerView.setAdapter(mAdapter);
-                } catch (JSONException e) {
-                    e.printStackTrace();
+                    mAdapter = nearbyAdapter(parseGoogleParse(result))
+
+                    recyclerView!!.layoutManager = mLayoutManager
+                    recyclerView!!.adapter = mAdapter
+                } catch (e: JSONException) {
+                    e.printStackTrace()
                 }
-
 
             }
         }
-    }
-
-    private ArrayList parseGoogleParse(final String url) throws JSONException {
-        ArrayList<content> temp;
-        temp = new ArrayList<>();
-        JSONObject jsonObject = new JSONObject(url);
-
-        if (jsonObject.has("results")) {
-            JSONArray jsonArray = new JSONArray("results");
-            content c = new content();
-            for (int i = 0; i < jsonArray.length(); i++) {
-
-                if (jsonArray.getJSONObject(i).has("name")) {
-                    c.setName(jsonArray.getJSONObject(i).optString("name"));
-                    if (jsonArray.getJSONObject(i).has("opening_hours")){
-                        if(jsonArray.getJSONObject(i).getJSONObject("opening_hours").has("open")){
-                            if (jsonArray.getJSONObject(i).getJSONObject("opening_hours").getString("open_now").equals("true")) {
-                                c.setOpenNow("YES");
-                            } else {
-                                c.setOpenNow("NO");
-                            }
-                        }
-                    }
-                    else {
-                        c.setOpenNow("Not Known");
-                    }
-                }
-            }
-
-            temp.add(c);
-        }
-        return temp;
     }
 }
---
+++ app/src/main/java/com/example/hp/hotelbooking/data/open_now.java
@@ -0,0 +1,8 @@
+package com.example.hp.hotelbooking.data;
+
+/**
+ * Created by kunai on 7/8/17.
+ */
+
+public class open_now {
+}
